6d17d352f7553381534516b7bdf8f00f141a85fe
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-26 15:10:35 +0100

    color config

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,10 +51,10 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     theme: {
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+        light: {
+          primary: '#234A5D',
+          accent: '#F5423A',
+          secondary: '#D1DCDF',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
