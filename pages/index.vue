7626c741dea7729a07448068dedc2bbf11643fb5
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-26 19:35:35 +0100

    usage of Banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,36 @@
 <template lang="pug">
-  v-row(justify='center' align='center')
-
+  .full 
+    .d-flex
+      Banner(:blabla='blabla', :image="image" :ctaText="ctaText" :link="link")
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Banner from "~/components/Banner/Banner";
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Banner
+  },
+  data() {
+    return {
+      blabla: {
+        title: "Une solution à tous vos projets web",
+        teaser: "Du front au back en passant par du SEO",
+        text:
+          "Développeur Web passionné, je suis constamment à la recherche d'améliorations, que ce soit à des fins personnelles ou professionnelles. Dans un monde poussé par l'innovation, la lecture quotidienne et la curiosité semblent obligatoires mais, pour moi c'est inné. J'ai toujours aimé plonger au plus profond des sujets qui me passionnent."
+      },
+      ctaText: "Découvrez mes projets",
+      link: "/projets",
+      image:
+        "https://images.unsplash.com/photo-1501127122-f385ca6ddd9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+    };
   }
 }
 </script>
+
+<style scoped>
+  .full {
+    width: 100%;
+    height: 100%;
+  }
+</style>
