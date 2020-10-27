07c98a273f21e07dd21e83b6f3345c4677a48234
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-27 19:31:03 +0100

    refacto Section component

---
+++ components/Section/Section.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .section(:style="{ background: color }")
+    a.section-anchor(id='Developpeur-Back-end')
+    .section-main
+      .section-wrapper(:class="{'flex-row-reverse': reverse}")
+        .section-right
+          .section-wrapper-right
+            Polygons(:inputs="polygonInputs" :color="blabla.textColor")
+        .section-left
+          BlaBla(:blabla="blabla")
+          ButtonProject(:text="ctaText" :href="link" :textColor="blabla.textColor")
+</template>
+
+<script>
+export default {
+  props: {
+    blabla: Object,
+    polygonInputs: Array,
+    ctaText: String,
+    link: String,
+    color: String,
+    reverse: Boolean
+  }
+}
+</script>
+
+<style>
+
+</style>
