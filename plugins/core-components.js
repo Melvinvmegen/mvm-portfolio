92de2aa978a9242f2b2d78ffd76d88eece7ede5b
Melvin van Megen <melvin.vmegen@gmail.com>
2020-11-02 10:48:49 +0100

    + core components

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -3,8 +3,9 @@ import BlaBla from '~/components/BlaBla/BlaBla'
 import ButtonProject from '~/components/Button/ButtonProject'
 import Polygons from '~/components/Polygons/Polygons'
 import Banner from "~/components/Banner/Banner";
+import TextHeader from '~/components/TextHeader/TextHeader'
 
-const components = { BlaBla, ButtonProject, Polygons, Banner }
+const components = { BlaBla, ButtonProject, Polygons, Banner, TextHeader }
 Object.entries(components).forEach(([name, component]) => {
   Vue.component(name, component)
 })
