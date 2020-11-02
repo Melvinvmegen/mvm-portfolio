c9bb281e7b0f72343a8b3223d8955c8f667fa7b3
Melvin van Megen <melvin.vmegen@gmail.com>
2020-11-02 10:52:13 +0100

    typed_js

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,10 +20,15 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/core-components'],
+  plugins: [
+  '~/plugins/core-components',
+  '~/plugins/typed-js',
+    { src: "@/plugins/aos", mode: "client" },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   // components: true,
@@ -43,7 +48,9 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+
+  },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
