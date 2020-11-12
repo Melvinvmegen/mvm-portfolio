8ee6f3374c364fe8b1084d14a1b74d4e051173ab
Melvin van Megen <melvin.vmegen@gmail.com>
2020-11-12 10:47:16 +0100

    cleanup polygons

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import BlaBla from '~/components/BlaBla/BlaBla'
 import ButtonProject from '~/components/Button/ButtonProject'
-import Polygons from '~/components/Polygons/Polygons'
 import Banner from "~/components/Banner/Banner"
 import TextHeader from '~/components/TextHeader/TextHeader'
 import Spinner from '~/components/Spinner/Spinner'
 import CTA from '~/components/CTA/CTA'
+import CompanyLogo from '~/components/CompanyLogo/CompanyLogo'
 
 
-const components = { BlaBla, ButtonProject, Polygons, Banner, TextHeader, Spinner, CTA }
+const components = { BlaBla, ButtonProject, Banner, TextHeader, Spinner, CTA, CompanyLogo }
 Object.entries(components).forEach(([name, component]) => {
   Vue.component(name, component)
 })
