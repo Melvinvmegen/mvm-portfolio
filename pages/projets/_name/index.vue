693f79982af0fe2ec00aace3f3dcbc489f4254ec
Melvin van Megen <melvin.vmegen@gmail.com>
2020-11-13 17:23:30 +0100

    improved projects

--- pages/projets/_name/index.vue
+++ pages/projets/_name/index.vue
@@ -3,15 +3,20 @@
     v-container
       TextHeader(:blabla="{title: title, teaser: teaser}" side="center" :color="true")
       Tags(:tags="tags")
-      v-img(:src="require(`~/assets/projects/${img}`)" contain)
+      v-img(:src="require(`~/assets/projects/${alt}/${notebook}`)" contain)
       .pb-15.pt-15
-        h2.subheader Le projet.
-        span {{ desc }}
+        h2.subheader Le projet...
+        span(v-html='desc')
     .section.secondary.align-start
       v-container
         .pb-15.pt-15
-          h2.subheader La solution.
-          span {{ desc }}
+          h2.subheader La solution...
+          span(v-html='solution')
+          br
+          .d-flex.justify-center.flex-column
+            .col-12.justify-center.align-center.d-flex.flex-column(v-for='projectImage in projectImages')
+              v-img.img-project( :src="require(`~/assets/projects/${alt}/${projectImage.img}.png`)" contain)
+              span.font-italic.text-center {{ projectImage.text }}
 </template>
 
 <script>
@@ -26,7 +31,10 @@ export default {
       title: this.$route.params.project.title,
       teaser: this.$route.params.project.teaser,
       desc: this.$route.params.project.desc,
+      solution: this.$route.params.project.solution,
       img: this.$route.params.project.img,
+      notebook: this.$route.params.project.notebook,
+      projectImages: this.$route.params.project.projectImages,
       alt: this.$route.params.project.alt,
       href: this.$route.params.project.href,
       btnText: this.$route.params.project.btnText,
@@ -41,5 +49,9 @@ export default {
     font-size: 40px;
     margin-bottom: 20px;
     line-height: 1.5;
+  }
+
+  .img-project {
+    margin: 40px 0;
   }
 </style>
